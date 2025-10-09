27f3c52d90488b10ee81d42f7369733df6a7b5c4
GotemBarbosa <gotembarbosagd@gmail.com>
2025-10-08 22:12:12 -0300

    versao inicial do componente

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Main from "./pages/main";
+import SelectedText from "./components/SelectedText";
 
 export default function Home() {
   return (
     <div>
       <Main />
+      <SelectedText text="Frase Muito legal dita por ai" user={{name:"aaaa", img:"https://i.imgur.com/3fA43t8.png"}} />
     </div>
   );
 }
