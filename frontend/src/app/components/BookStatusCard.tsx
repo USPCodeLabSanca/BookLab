c28898f1a67204c67990c721b74dfca5c67fcd7b
c-capocasali <caio.capocasali@usp.br>
2025-10-08 23:52:48 -0300

    feature/Adição dos icones

--- frontend/src/app/components/BookStatusCard.tsx
+++ frontend/src/app/components/BookStatusCard.tsx
@@ -1,3 +1,8 @@
+import { GoHourglass } from "react-icons/go"
+import { MdOutlineTextFields } from "react-icons/md";
+import { FaPenNib } from "react-icons/fa6";
+
+
 interface BookStatusCardProps {
     timeLeft: string;
     lettersLeft: number;
@@ -15,17 +20,19 @@ export default function BookStatusCard({ timeLeft, lettersLeft, bookTheme }: Boo
                     Tempo restante para finalizar o livro:
                 </h2>
                 <div className="flex items-center justify-center gap-2">
+                    <GoHourglass />
                     <span className="bg-[#51433E] px-4 py-1 rounded-full text-lg font-semibold">
                         {timeLeft}
                     </span>
                 </div>
             </div>
 
-            <div className="flex justify-between items-center text-center">
+            <div className="flex justify-between gap-x-7 items-center text-center">
                 {/* Letras Restantes */}
                 <div>
                     <h3 className="text-sm text-gray-300 mb-2">Suas letras restantes:</h3>
                     <div className="flex items-center justify-center gap-2">
+                        <MdOutlineTextFields />
                         <span className="bg-[#51433E] px-4 py-1 rounded-full text-lg font-semibold">
                             {lettersLeft} letras
                         </span>
@@ -36,6 +43,7 @@ export default function BookStatusCard({ timeLeft, lettersLeft, bookTheme }: Boo
                 <div>
                     <h3 className="text-sm text-gray-300 mb-2">Tema do livro:</h3>
                     <div className="flex items-center justify-center gap-2">
+                        <FaPenNib />
                         <span className="bg-[#51433E] px-4 py-1 rounded-full text-lg font-semibold">
                             {bookTheme}
                         </span>
