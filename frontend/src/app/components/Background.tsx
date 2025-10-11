73b2eea1171952898be067444c620b27f7a27f70
Ícaro Ramos Caye <caye2018khan@gmail.com>
2025-10-11 18:57:38 -0300

    Criação dos arquivos dos componentes

---
+++ frontend/src/app/components/Background.tsx
@@ -0,0 +1,7 @@
+export default function Background(){
+    return(
+        <div className="flex-1 bg-amber-100 h-full rounded-l-2xl border-r-4 border-amber-800 flex items-center justify-center">
+            <p className="text-gray-500 italic">Área do texto</p>
+        </div>
+    )
+}
---
+++ frontend/src/app/components/InfoPanel.tsx
@@ -0,0 +1,7 @@
+export default function InfoPanel() {
+  return (
+    <div className="w-full md:w-80 bg-stone-900 h-full rounded-r-2xl flex items-center justify-center">
+      <p className="text-stone-300 italic">Painel de informações</p>
+    </div>
+  );
+}
