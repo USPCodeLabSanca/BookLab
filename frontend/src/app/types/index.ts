570e7e3dbdd6d9ffaad70cb6360498c9e5f13039
c-capocasali <caio.capocasali@usp.br>
2025-10-13 15:22:39 -0300

    feature: Adicionado componente autores

--- frontend/src/app/types/index.ts
+++ frontend/src/app/types/index.ts
@@ -1,6 +1,12 @@
 //exemplo de tipagem (apenas para anotação)
-export default interface tipo{
-    nome: string, 
-    text: string, 
+export default interface tipo {
+    nome: string,
+    text: string,
     idade: Number
+}
+
+export interface Author {
+    id: number;
+    name: string;
+    avatarUrl: string;
 }
